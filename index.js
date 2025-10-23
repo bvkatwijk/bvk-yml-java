83f92d23870ff59cbc9274362c4b6948b54fdb48
Boris van Katwijk <bvkatwijk@gmail.com>
2025-10-23 10:06:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,70 @@ export function ymlToJava(yml) {
     return "ImmutableMap.of();";
 }
 
+
+function getDefault() {
+  return `---
+# A sample yaml file
+company: spacelift
+domain:
+ - devops
+ - devsecops
+tutorial:
+  - yaml:
+      name: "YAML Ain't Markup Language"
+      type: awesome
+      born: 2001
+  - json:
+      name: JavaScript Object Notation
+      type: great
+      born: 2001
+  - xml:
+      name: Extensible Markup Language
+      type: good
+      born: 1996
+author: omkarbirade
+published: true
+`;
+}
+
+function parse(yml) {
+  try {
+    return {
+      success: true,
+      value: ymlToJava(yml),
+    };
+  } catch (e) {
+    return {
+      success: false,
+      value: e,
+    };
+  }
+}
+
+function getWarningCard() {
+  return document.getElementById("warning-card");
+}
+
+function clearWarning() {
+  getWarningCard().innerHTML = null;
+}
+
+function clearOutput() {
+  getOutputArea().innerHTML = null;
+}
+
+function setWarning(error) {
+  getWarningCard().innerHTML =
+    '<div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>Parsing Error</strong>\n' +
+    error +
+    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    ymlToJava: ymlToJava,
+    renderAttr,
+    renderChild,
+    renderText
+  };
+}
